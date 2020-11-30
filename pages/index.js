e63239eb0e05df3cdd3415218ce7fbdcdc0e828a
SDaian <scuarissid@gmail.com>
2020-11-30 15:09:10 -0300

    Added auth via auth0 package

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useContext } from 'react';
 
 import Navbar from '../components/Navbar'
+import Todo from '../components/Todo';
+import auth0 from './api/utils/auth0';
 
-import {table, minifyRecords} from './api/utils/Airtable';
+import { table, minifyRecords } from './api/utils/Airtable';
 
-export default function Home({initialTodos}) {
+import { TodosContext } from '../contexts/TodosContext';
+import { initAuth0 } from '@auth0/nextjs-auth0';
+
+export default function Home({ initialTodos, user }) {
+
+  const {todos, setTodos} = useContext(TodosContext);
+  console.log(user);
+
+  useEffect(() => {
+    setTodos(initialTodos)
+  }, [])
   console.log(`TO-DO: ${initialTodos}`);
   return (
     <div>
@@ -13,9 +26,12 @@ export default function Home({initialTodos}) {
         <title>Authenticated TODO App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <Navbar user={user} />
       <main>
         <h1>Todo App</h1>
+        <ul>
+          { todos && todos.map(todo => <Todo key={todo.id} todo={todo} />)}
+        </ul>
       </main>
       <footer>
       </footer>
@@ -24,11 +40,13 @@ export default function Home({initialTodos}) {
 }
 
 export async function getServerSideProps(context) {
+  const session = await auth0.getSession(context.req);
   try {
     const todos = await table.select({}).firstPage();
     return {
       props: {
-        initialTodos: minifyRecords(todos)
+        initialTodos: minifyRecords(todos),
+        user: session?.user || null
       }
     }
   } catch (err) {
